f21ecd5ed691f6192088aba31d7733a8551f9920
Luis-Program <luiscaceres217@hotmail.es>
2021-07-20 08:48:36 -0600

    Completed

---
+++ src/main/java/com/mycompany/chess/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.com.mycompany.chess;
+
+import main.java.com.mycompany.chess.Jframe.home;
+
+
+/**
+ *
+ * @author luisc
+ */
+public class Main {
+    
+    public static void main(String args[]) {
+        home h = new home();
+        h.setVisible(true);
+    }
+    
+}
